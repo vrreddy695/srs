0d2deb00bfa47ad3d2c723ab876a8c551337f6e9
vrreddy695 <ramireddy2006@gmail.com>
2018-11-04 21:29:49 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="startifiedRS",
+    version="0.0.1",
+    author="Venkatramireddy Koppula",
+    author_email="venkatrami.reddy@latentview.com",
+    description="Startied random sample from the dataframe",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
