462afe1e2a0dd4ff4e074ffff43fb5d623a7680a
vrreddy695 <ramireddy2006@gmail.com>
2018-11-04 21:27:46 -0800

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from  setuptools import setup
-
-setup(
-    name='startifiedRS',
-    version='0.0.1',
-    descripiton='select a startified_random_smaple from a population dataframe with sampe proportions of sample elments for each of starata of the population',
-    py_modules=["startifiedRS"],
-    package_dir={'': 'src'},
-)
