d1fe69a17179075ce9e52b2e45892c2d1a7017a7
Venkatramireddy Koppula <vrreddy@Venkatramireddys-MacBook-Pro.local>
2018-11-21 20:42:14 -0800

    first commit

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="startifiedRS",
+    name="srs",
     version="0.0.1",
     author="Venkatramireddy Koppula",
     author_email="venkatrami.reddy@latentview.com",
     description="Startied random sample from the dataframe",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     url="https://github.com/vrreddy695/startifiedRS",
     packages=setuptools.find_packages(),
     classifiers=[
---
+++ srs/startified_sample.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+def samp(dsn, stratas, sample_size):
+    """
+    parameters
+    dsn: population dataset
+    stratas: column nsmaes as list format ex: ['strata1','strata2','strata3....']
+    sample_size: how much percentage you want as a sample like 0.05, 0.10, 0.25...
+    """
+    freq=dsn.groupby(stratas).size().reset_index(name='counts')
+    freq['pct']=freq['counts']/data.shape[0]
+    samp_size_num=(data.shape[0])*sample_size
+    freq['sample']=(freq['pct']*samp_size_num).apply(np.round)
+    freq=freq[freq['sample']>0] # if the sample is less than 0
+    sample=pd.DataFrame()
+    for index,row in freq.iterrows():
+        freq_bkt=freq.loc[freq.index==index]
+        result = pd.merge(data, freq_bkt, on=stratas)
+        samp=result.sample(n=int(row['sample']), random_state=99)
+        sample=sample.append(samp)
+    sample.drop(['counts','pct', 'sample'], axis=1,inplace=True)
+    return sample
